85d09f8b4aded9f7590fb0c3d5072d14bcc95c89
Wiston Venera Macías <wisvem@hotmail.com>
2022-04-17 01:26:11 +0000

    - Fixed urls - Fixed styles

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', Home.as_view()),
-    path('register/', Register.as_view()),
-    path('login/', Login.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('register/', Register.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
