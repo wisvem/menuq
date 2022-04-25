dbcae6e1449430949ad1edfbdf0ed4546e092ae5
Wiston Venera Macías <wisvem@hotmail.com>
2022-04-25 16:59:06 +0000

    Tweak style

--- apps/menus/static/js/sortable-table.js
+++ apps/menus/static/js/sortable-table.js
@@ -1,6 +1,6 @@
-$('#detail-form').sortable({
+$('tbody').sortable({
   axis: 'y',
-  handle: '.fa-grip-vertical' 
+  handle: '.fa-grip-vertical',
 });
 
 
@@ -9,7 +9,6 @@ $('form').submit(function () {
   $('#detail-form').find('tr:visible').each(function (index) {
     // This should target the order field within the row
     if ($(this).find('input[type="hidden"]')[2].hasAttribute('value')) {
-      console.log('aqui jue');
       $(this).find('input[type="hidden"]')[0].value = index;
     } else {
       $(this).find('input[type="hidden"]')[0].removeAttribute('value');
